235a27efecc6270472994060b72be844807c9d34
Rémi Charbonneau <remi.charbonneau@gmail.com>
2016-02-17 19:40:57 -0500

    Change the design data to more recent version.

--- RReplay/Design/DesignReplayRepository.cs
+++ RReplay/Design/DesignReplayRepository.cs
@@ -14,10 +14,11 @@ namespace RReplay.Design
             var errorParsing = new List<Tuple<string, string>>();
 
             // TODO Find how to not hardcode design time data
-            replayCollection.Add(new Replay(@"F:\Dev\RedReplay\RedReplay\RReplay\Design\Data\1vs_1_schlamschlacht_ranked_fr_Horus.wargamerpl2"));
-            replayCollection.Add(new Replay(@"F:\Dev\RedReplay\RedReplay\RReplay\Design\Data\TKBLU.wargamerpl2"));
-            replayCollection.Add(new Replay(@"F:\Dev\RedReplay\RedReplay\RReplay\Design\Data\TEAM_Killer_replay_2014-06-14_23-55-58.wargamerpl2"));
-            replayCollection.Add(new Replay(@"F:\Dev\RedReplay\RedReplay\RReplay\Design\Data\replay_2014-10-26_16-27-14.wargamerpl2"));
+            replayCollection.Add(new Replay(@"F:\Dev\RedReplay\RedReplay\RReplay\Design\Data\czech_before_1985_naschlap.wargamerpl2"));
+            replayCollection.Add(new Replay(@"F:\Dev\RedReplay\RedReplay\RReplay\Design\Data\replay_2016-02-10_00-02-46.wargamerpl2"));
+            replayCollection.Add(new Replay(@"F:\Dev\RedReplay\RedReplay\RReplay\Design\Data\replay_2015-10-05_16-48-18.wargamerpl2"));
+            replayCollection.Add(new Replay(@"F:\Dev\RedReplay\RedReplay\RReplay\Design\Data\Sotko_Anti_Chinese_Cheese_Defense.wargamerpl2"));
+            replayCollection.Add(new Replay(@"F:\Dev\RedReplay\RedReplay\RReplay\Design\Data\patton 1.wargamerpl2"));
 
             var collec = from replay in replayCollection
                          from players in replay.Players
